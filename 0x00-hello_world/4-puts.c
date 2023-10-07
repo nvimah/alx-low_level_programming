008c68c25f18131d1df390b60f34708578ef3731
nvimah <navion8759@gmail.com>
2023-10-07 05:24:19 -0700

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - 4-puts.c
+ *description - write a C program that prints exactly
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle/n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**
+ * main - 5-printtf.c
+ * description - Write a C program that prints exactly  with proper grammar,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, /n");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 }
