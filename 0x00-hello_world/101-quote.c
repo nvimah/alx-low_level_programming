71eedf315e50a43e622bd36421032efa22266577
nvimah <navion8759@gmail.com>
2023-10-07 06:16:32 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <unistd.h>
-Int main(void)
+/**
+ * *main - 101-quote.c
+ * description - write a c program that prints exactly 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-		write(2,
-		“and that piece of art is useful\” - Dora Korpar,  2015 - 10 - 19\n”,
-		59);
+		write(2,“and that piece of art is useful\” - Dora Korpar,  2015 - 10 - 19\n”,59);
 
 		return (1);
 }
